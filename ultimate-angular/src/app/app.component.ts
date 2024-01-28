f8f6b894a070a6289990ed06146b1d7177e6d416
ballaschottner <76956521+ballaschottner@users.noreply.github.com>
2024-01-28 15:14:50 +0100

    @Getting started

--- ultimate-angular/src/app/app.component.ts
+++ ultimate-angular/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
+ @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent {
-  title = 'ultimate-angular';
-}
+  styleUrls: ['app.component.scss'],
+
+ })
+ export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular'
+  }
+ }
