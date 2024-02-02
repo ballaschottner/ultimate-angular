d552660645c922fefc59e308203370726cd660b8
ballaschottner <76956521+ballaschottner@users.noreply.github.com>
2024-02-02 23:58:33 +0100

    Feature modules with @NgModule

--- ultimate-angular/src/app/app.module.ts
+++ ultimate-angular/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, CommonModule, FormsModule],
+    imports: [BrowserModule, CommonModule, PassengerDashboardModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
