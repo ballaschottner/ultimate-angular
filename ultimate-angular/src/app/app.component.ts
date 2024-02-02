d6d099b13b7f98527a1bc7ac5fab152f0cfecd55
ballaschottner <76956521+ballaschottner@users.noreply.github.com>
2024-02-02 22:20:07 +0100

    ngFor and iterating collections

--- ultimate-angular/src/app/app.component.ts
+++ ultimate-angular/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Passengers {
+  id: number,
+  fullName: string,
+  checkedIn: boolean
+}
  @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,4 +17,20 @@ import { Component } from '@angular/core';
   handleChange(event: any) {
     this.name = event.target.value;
   }
+
+  passengers: Passengers[] = [{
+    id: 1,
+    fullName: 'Stephen',
+    checkedIn: true
+  },
+  {
+    id: 2,
+    fullName: 'Rose',
+    checkedIn: false
+  },
+  {
+    id: 3,
+    fullName: 'James',
+    checkedIn: true
+  },]
  }
