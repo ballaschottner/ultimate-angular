bac0408ffcb7f7e013c60243c8ed146cce2b5a52
ballaschottner <76956521+ballaschottner@users.noreply.github.com>
2024-02-03 23:11:25 +0100

    @Services, Http and Observables

--- ultimate-angular/src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ ultimate-angular/src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpRequest } from "@angular/common/http";
 import { Passenger, Passengers } from "./models/passenger.interface";
 import { Observable, map } from "rxjs";
 
@@ -17,6 +17,12 @@ export class PassengerDashboardService {
     }
 
     updatePassenger(passenger: Passengers): Observable<Passengers> {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+      /* let options = new HttpRequest({
+        headers: headers
+      }); */
       return this.http
         .put(`${PASSENGER_API}/${passenger.id}`, passenger)
         .pipe(
