acf0c5933d475ad1ffd1c37b568cec0719d033ff
ballaschottner <76956521+ballaschottner@users.noreply.github.com>
2024-02-10 23:16:43 +0100

    save

--- ultimate-angular-pro/src/app/app.module.ts
+++ ultimate-angular-pro/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthFormModule } from './auth-form/auth-form.module';
 
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardDirective,
+    TooltipDirective
   ],
   imports: [
     BrowserModule,
